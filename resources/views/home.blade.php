2f6e602acfb40390bc19f545245a59ab15e7f010
ehroy <ahmadroy092@gmail.com>
2024-06-30 15:03:56 +0700

    Update Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <a href="">
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
         </a>
-        <ul class="flex justify-end space-x-4 font-bold gap-12">
+        <ul class="hidden sm:flex justify-end space-x-4 font-bold gap-12">
             <li>
                 <a href="">Home</a>
             </li>
@@ -18,6 +18,12 @@
                 <a href="">Contact</a>
             </li>
         </ul>
+        <i class=" block sm:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+              
+        </i>
     </div>
 </nav>
 <section class="mt-8">
@@ -48,15 +54,117 @@
     </div>
 </section>
 <section>
-    <div class="container mx-auto col-span-12 bg-red-200">
-        <div>
-            <h3 class="">
-                Smooth Touch 
-            </h3>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem culpa corporis libero voluptatibus consequatur consectetur cumque aperiam pariatur quisquam, sed architecto impedit rem molestiae et nihil cum omnis, ut officiis!
-            </p>
+    <div class="container mx-auto grid grid-cols-12 gap-4 mt-12 mb-12">
+        <div class="col-start-1 col-span-4 ">
+            <div class="flex flex-col p-4">
+                <h4 class="mb-2">
+                    Smooth Touch
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+            </div>
+            <div class="flex flex-col p-4">
+                <h4 class="mb-2">
+                    Smooth Touch
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+            </div>
+            <div class="flex flex-col p-4">
+                <h4 class="mb-2">
+                    Smooth Touch
+                </h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+            </div>
         </div>
+        <div class="col-start-5 col-span-4 ">
+            <div class="items-center  px-16">
+                <img class="w-[650px] h-[500px]" src="https://demo.gethugothemes.com/vex/site/images/watch.png" alt="">   
+            </div> 
+        </div>
+        <div class="col-start-9 col-span-4">
+                <div class="flex flex-col p-4">
+                    <h4 class="mb-2">
+                        Smooth Touch
+                    </h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+                </div>
+                <div class="flex flex-col p-4">
+                    <h4 class="mb-2">
+                        Smooth Touch
+                    </h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+                </div>
+                <div class="flex flex-col p-4">
+                    <h4 class="mb-2">
+                        Smooth Touch
+                    </h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, inventore?</p>
+                </div>
+           </div>
+    </div>
+</section>
+<div class=" mx-auto flex flex-col p-4 sm:flex-row sm:left-0 sm:right-0 overflow-hidden bg-orange-400 left-0 right-0">
+
+    <div class="items-center  px-16">
+        <img class="w-[650px] h-[500px]" src="https://demo.gethugothemes.com/vex/site/images/watch.png" alt="">
+        
+    </div>
+    <div class="mx-auto">
+        <h1 class="text-5xl p-2 font-serif text-center sm:text-8xl sm:text-left font-bold">
+            Believe in Technology, Make the change
+        </h1>
+        <p class="text-lg p-2 mt-8 text-center sm:text-left">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, modi fugit in veritatis labore perferendis. Minima hic at, nostrum nihil!
+        </p>
+     
+    </div>
+</div>
+<div>
+
+</div>
+<div class="m-12 justify-center text-center font-semibold">
+    <h2>
+        Why Choose Apple Watch ?
+    </h2>
+</div>
+</section>
+<section>
+    <div class="container mx-auto flex flex-col p-4 sm:flex-row sm:left-0 sm:right-0 overflow-hidden  left-0 right-0">
+
+        <div class="items-center  px-16">
+            <img class="w-[650px] h-[500px]" src="https://demo.gethugothemes.com/vex/site/images/watch.png" alt="">
+            
+        </div>
+        <div class="mx-auto">
+            <h1 class="text-5xl p-2 font-serif text-center sm:text-8xl sm:text-left font-bold">
+                Believe in Technology, Make the change
+            </h1>
+            <p class="text-lg p-2 mt-8 text-center sm:text-left">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, modi fugit in veritatis labore perferendis. Minima hic at, nostrum nihil!
+            </p>
+         
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container mx-auto flex flex-col p-4 sm:flex-row sm:left-0 sm:right-0 overflow-hidden  left-0 right-0">
+        <div class="mx-auto">
+            <h1 class="text-5xl p-2 font-serif text-center sm:text-8xl sm:text-left font-bold">
+                Believe in Technology, Make the change
+            </h1>
+            <p class="text-lg p-2 mt-8 text-center sm:text-left">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, modi fugit in veritatis labore perferendis. Minima hic at, nostrum nihil!
+            </p>
+         
+        </div>
+        <div class="items-center  px-16">
+            <img class="w-[650px] h-[500px]" src="https://demo.gethugothemes.com/vex/site/images/watch.png" alt="">
+            
+        </div>
+    </div>
+    <div class="m-12 justify-center text-center font-semibold">
+        <h2>
+            Check My Product List ?
+        </h2>
     </div>
 </section>
 @section('content')
